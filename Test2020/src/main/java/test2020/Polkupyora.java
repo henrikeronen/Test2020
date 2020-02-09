52715e877e9fa4fa947d913256abae4270ff27a5
Henrik <Henrik@DESKTOP-H6EHSVH>
2020-02-09 16:32:39 +0200

    IKulkuneuvo rajapinta lisätty

---
+++ Test2020/src/main/java/test2020/Polkupyora.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test2020;
+
+/**
+ *
+ * @author Henrik
+ */
+public class Polkupyora extends Kulkuneuvo {
+
+    @Override
+    public void tulostaTiedot() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
